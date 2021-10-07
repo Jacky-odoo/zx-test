32c41f4cffc805da3164b89ea58e3c3195cf4366
Nejc Gale <nejc@luxim.si>
2021-10-07 16:41:05 +0200

    Work in progress

--- zx-pot-wiz/models/thecode.py
+++ zx-pot-wiz/models/thecode.py
@@ -6,19 +6,20 @@ from odoo import models, fields, api
 home = os.path.expanduser("~")  # get root directory
 filenames = glob.glob(home+"/**/*.pot")  # list of all .pot files in the root directory
 potsrc = home+'/odoo/zx-test/zx-global-translate/i18n/zx-global-translate-generated.pot'  # .pot file location
+state = fields.Selection([
+	('waiting', 'Waiting'),
+	('ready', 'Ready'),
+	('done', 'Done'),
+], required=True, default='waiting')
 
 class LangWizz(models.TransientModel):
 	_name = 'lang.wizz'
 
-	potFile = fields.Binary(string='pot file')
-	state = fields.Selection([
-		('waiting', 'Waiting'),
-		('ready', 'Ready'),
-		('done', 'Done'),
-	], required=True, default='waiting')
+	# potFile = fields.Binary(string='pot file')
 
 	@api.multi
 	def get_pot(self):
+		self.ensure_one()
 		print(home)
 		with open(potsrc, 'w', encoding="utf-8") as f:
 			for file in filenames:
@@ -30,6 +31,7 @@ class LangWizz(models.TransientModel):
 # code snipet for downloading file
 	@api.multi
 	def dl_pot(self):
+		self.ensure_one()
 		for rec in self:
 			rec.write({'state': 'done'})
 		return {
