9792155cf56f5ee5689f5dec06c650c2172c452a
Nejc Gale <nejc@luxim.si>
2021-10-07 16:52:40 +0200

    Work in progress

--- zx-pot-wiz/models/thecode.py
+++ zx-pot-wiz/models/thecode.py
@@ -6,11 +6,11 @@ from odoo import models, fields, api
 home = os.path.expanduser("~")  # get root directory
 filenames = glob.glob(home+"/**/*.pot")  # list of all .pot files in the root directory
 potsrc = home+'/odoo/zx-test/zx-global-translate/i18n/zx-global-translate-generated.pot'  # .pot file location
-state = fields.Selection([
-	('waiting', 'Waiting'),
-	('ready', 'Ready'),
-	('done', 'Done'),
-], required=True, default='waiting')
+# state = fields.Selection([
+# 	('waiting', 'Waiting'),
+# 	('ready', 'Ready'),
+# 	('done', 'Done'),
+# ], required=True, default='waiting')
 
 class LangWizz(models.TransientModel):
 	_name = 'lang.wizz'
@@ -25,15 +25,15 @@ class LangWizz(models.TransientModel):
 			for file in filenames:
 				with open(file, encoding="utf-8") as infile:
 					f.write(str(infile.read()) + '\n')
-		for rec in self:
-			rec.write({'state': 'ready'})
+		# for rec in self:
+		# 	rec.state = 'ready'
 
 # code snipet for downloading file
 	@api.multi
 	def dl_pot(self):
 		self.ensure_one()
-		for rec in self:
-			rec.write({'state': 'done'})
+		# for rec in self:
+		# 	rec.state = 'done'
 		return {
 			'type': 'ir.actions.act_url',
 			'url': str(potsrc+'download=true'),
