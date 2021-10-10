0e23898386e6530ba3ce88ab47682d8d0ab7e3a5
Nejc G <91312708+nejc86@users.noreply.github.com>
2021-10-10 16:44:40 +0200

    Revert "Patching for 14.0"
    This reverts commit 67ffb14ecd8a641a644981f1881506bb64e80fb7.

--- zx-pot-wiz/models/thecode.py
+++ zx-pot-wiz/models/thecode.py
@@ -3,10 +3,9 @@
 import os
 import glob
 from odoo import models, fields, api
-
-home = os.path.expanduser("~/../odoo14")  # get root directory
-potsrc = home + '/zx-test/zx-global-translate/i18n/zx-global-translate-generated.pot'  # .pot file location
+home = os.path.expanduser("~")  # get root directory
 filenames = glob.glob(home+"/**/*.pot")  # list of all .pot files in the root directory
+potsrc = home+'/odoo/zx-test/zx-global-translate/i18n/zx-global-translate-generated.pot'  # .pot file location
 # state = fields.Selection([
 # 	('waiting', 'Waiting'),
 # 	('ready', 'Ready'),
@@ -18,11 +17,10 @@ class LangWizz(models.TransientModel):
 
 	# potFile = fields.Binary(string='pot file')
 
+	@api.multi
 	def get_pot(self):
-		# self.ensure_one()
-		base_url = self.env['ir.config_parameter'].get_param('web.base.url')
-		print(potsrc)
-		# print(str(glob.glob(home+"/**/*.pot").pop(0)))
+		self.ensure_one()
+		print(home)
 		with open(potsrc, 'w', encoding="utf-8") as f:
 			for file in filenames:
 				with open(file, encoding="utf-8") as infile:
@@ -31,12 +29,13 @@ class LangWizz(models.TransientModel):
 		# 	rec.state = 'ready'
 
 # code snipet for downloading file
+	@api.multi
 	def dl_pot(self):
 		self.ensure_one()
 		# for rec in self:
 		# 	rec.state = 'done'
 		return {
 			'type': 'ir.actions.act_url',
-			'url': potsrc+str('?download=true'),
+			'url': potsrc,
 			'target': 'self',
 		}
